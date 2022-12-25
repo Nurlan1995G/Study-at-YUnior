4850ef27bcbc31f7e7a9cf31914afe42c3c263f0
Nurlan19958 <www.nurlan1995@mail.ru>
2022-12-25 17:12:13 +0400

    Study at YUnior

---
+++ Tasks1/Tasks1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Tasks    // Задача    Попрактикуйтесь в создании переменных, объявить 10 переменных разных типов.
+                   //Напоминание: переменные именуются с маленькой буквы, если название состоит из нескольких слов,
+                   //то комбинируем их следующим образом - названиеПеременной.
+                   //Также имя всегда должно отражать суть того, что хранит переменная.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameCompany = "Афон";
+            uint staffCount = 1200;  // количество сотрудников
+            float avarageSalary = 31.5f; // средняя зарплата
+            byte transactionsWeek = 146;    //выполнено операций за неделю
+            long annualRevenue = 50002000021;  //годовая выручка
+            char companySimbols = 'A';   //символ компании
+            int productCount = 12500;   //количество продукта на продажу
+            short planMonth = 1500;   //план операций на месяц
+            bool planToPerform;   // выполнят ли план
+            double companyDividends = 2300430000.000;  //дивиденды компании
+        }
+    }
+}
+
