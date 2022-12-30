094c01f5adca46cbbd6feb1de5ae41b26060aede
Nurlan19958 <www.nurlan1995@mail.ru>
2022-12-30 09:14:19 +0400

    Study at YUnior

--- Tasks1/Tasks1/Program.cs
+++ Tasks1/Tasks1/Program.cs
@@ -8,12 +8,12 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
-            int gold;           
-            int crystalPrice = 5;  
-            int crystalCount;
+            int gold;
+            int crystalPrice = 5;
+            int crystalsCount;
 
-           Console.WriteLine("Доброго времени суток, стоимость одного кристалла равна " + crystalPrice + 
-               " золота");
+            Console.WriteLine("Доброго времени суток, стоимость одного кристалла равна " + crystalPrice +
+                " золота");
             Console.Write("Введите, сколько у вас золота: ");
             gold = Convert.ToInt32(Console.ReadLine());
             Console.Write("Сколько кристаллов вы хотите приобрести? ");
@@ -22,6 +22,8 @@ namespace Tasks
             gold -= crystalCount * crystalPrice;
 
             Console.WriteLine("У вас в ящике находится " + crystalCount + " кристалла и " + gold + " золота");
+
+
         }
     }
 }
