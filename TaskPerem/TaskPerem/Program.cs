632048b9eebd3bf5549bed35abf6b265b9093e8f
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-03 17:30:32 +0400

    Peremenie
    Задание на переменные, 10

---
+++ TaskPerem/TaskPerem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Tasks        
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titleCompany = "Афон";  
+            byte staffsCount = 200; 
+            char logoCompany = 'A'; 
+            float averageSalary = 31000.550f;  
+            long annualRevenue = 2550000000;  
+            short ordersMonth = 1500;  
+            int expensesCompany = 1700000000;   
+            bool isProfitCompany;   
+            double transactionsCost = 13599.99;    
+            sbyte employeeGrowth = -5;
+        }
+    }
+}
--- TaskPerem/TaskPerem/obj/Debug/net6.0/TaskPerem.AssemblyInfo.cs
+++ TaskPerem/TaskPerem/obj/Debug/net6.0/TaskPerem.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Tasks1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TaskPerem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Tasks1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Tasks1")]
+[assembly: System.Reflection.AssemblyProductAttribute("TaskPerem")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TaskPerem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
