0b8cbd88d5d888be09d12967caf854773a30ed95
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-04 17:03:02 +0400

    Poliklinika
    ДЗ по поликлинике

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 
-
 namespace TaskC     
 {
     class Task
     {
         static void Main(string[] args)
         {
-            int hoursTimeCount = 60;
+            int minutesInHour = 60;
             int patientsCount;  
             int inspectionTime = 10; 
             int expectationTime;
             Console.Write("Введите количество пациентов: ");
             patientsCount = Convert.ToInt32(Console.ReadLine());
             expectationTime = patientsCount * inspectionTime;
-            int waitingHours = expectationTime / hoursTimeCount;
-            int waitingMinutes = expectationTime % hoursTimeCount;
+            int waitingHours = expectationTime / minutesInHour;
+            int waitingMinutes = expectationTime % minutesInHour;
             Console.WriteLine($"Вы должны отстоять в очереди {waitingHours} часа {waitingMinutes} минут");
         }
     }
