730675e42da62b6916c7dcb6756fdbd447c57e78
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-04 10:02:54 +0400

    Poliklinika
    ДЗ по поликлинике

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace TaskC      //Вы заходите в поликлинику и видите огромную очередь из старушек, вам нужно рассчитать
+                     //время ожидания в очереди.     Формально: Пользователь вводит кол-во людей в очереди.
+                     //Фиксированное время приема одного человека всегда равно 10 минутам.Пример ввода:
+                     //Введите кол-во старушек: 14   Пример вывода: "Вы должны отстоять в очереди 2 часа и 20 минут."
+{
+    class Task
+    {
+        static void Main(string[] args)
+        {
+            int patientsCount = 1;  
+            int expectationTime = 10;  
+            Console.Write("Введите количество пациентов: ");
+            patientsCount = Convert.ToInt32(Console.ReadLine());
+            patientsCount *= expectationTime;
+            int chas = patientsCount / 60;
+            int min = patientsCount % 60;
+            Console.WriteLine($"Вы должны отстоять в очереди {chas} часа {min} минут");
+        }
+    }
+}
+
