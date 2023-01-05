5cb03f37dcd2bcf688d3a467d1c61ccca1a978e8
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-05 20:06:43 +0400

    Picture
    ДЗ по картинкам с данными

---
+++ TaskPicture/TaskPicture/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TaskPicture      
+{
+    class Task
+    {
+        static void Main(string[] args)
+        {
+            int picturesCount = 52;  
+            int rowPictures = 3;   
+            int fullRow;    
+            int remainsRow;
+            Console.WriteLine($"Нам данно {picturesCount} картинки, которые выводятся в {rowPictures} ряда, сколько получится рядов?");
+            fullRow = picturesCount / rowPictures;
+            remainsRow = picturesCount % rowPictures;
+            Console.WriteLine($"На экране выведется полных {fullRow} рядов и {remainsRow} сверх меры");
+
+        }
+    }
+}
