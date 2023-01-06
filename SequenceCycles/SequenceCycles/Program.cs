edc90535f2b314fc4184bcf5a1e94e67a66f6428
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-06 18:04:21 +0400

    SequenceCycles
    ДЗ по циклам - 3е задание

---
+++ SequenceCycles/SequenceCycles/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SequenceCycles
+{
+    class Cycles        
+    {
+        static void Main(string[] args)
+        {
+            int weightproducts = 100;
+            int enumeration = 7;
+
+            for(int i = 5; i < weightproducts; i+= enumeration)
+                Console.WriteLine(i);
+        }
+    }
+}
