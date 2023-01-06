41449adb22052585f5ee5e1253f92415dbae5918
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-06 17:08:48 +0400

    CiclesTask
    ДЗ по циклам

---
+++ CiclesTask/CiclesTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CiclesTask
+{
+    class Task           
+    {
+        static void Main(string[] args)
+        {
+            string nameWebsite = "Azura";
+            string textToPrint;
+            int repeatData;
+
+            Console.WriteLine($"Добро пожаловать на наш сайт {nameWebsite}, здесь вы можете написать текст в неограниченом кол-ве" +
+                $" раз и отправить на печать");
+            Console.Write("Введите текст: ");
+            textToPrint = Console.ReadLine();
+            Console.Write("Введите сколько раз нужно вывести текст: ");
+            repeatData = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < repeatData; i++)
+                Console.WriteLine(textToPrint);
+        }
+    }
+}
