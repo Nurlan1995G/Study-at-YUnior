cd5192078ba067a950ea73a15ee5e6a94b7a48e9
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-08 16:26:38 +0400

    RandomCycles
    ДЗ по циклам - 4ое задание

---
+++ RandomCycles/RandomCycles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RandonCycles
+{
+    class Cycles        
+    { 
+        static void Main(string[] args)
+        {
+            int numbersSum = 0;
+            Random random = new Random();
+            int numberRand;
+            int numberMultiple;
+
+            numberRand = random.Next(0, 100);
+           
+            Console.Write($"Введите кратное число 3 или 5: ");
+            numberMultiple = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < numberRand; i++)
+            {
+                if(i % numberMultiple == 0)
+                {
+                    numbersSum += i;
+                }
+            }
+            Console.WriteLine($"Рандомное число выпало {numberRand}");
+            Console.WriteLine($"Сумма чисел кратной {numberMultiple} = {numbersSum}");
+
+        }
+    }
+}
