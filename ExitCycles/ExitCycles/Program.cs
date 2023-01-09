8454a4000e233c639fdfe8eb221ae1ecd57bdfe0
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-09 17:16:39 +0400

    ExitCycles
    ДЗ по циклам - вроде 2ое задание

--- ExitCycles/ExitCycles/Program.cs
+++ ExitCycles/ExitCycles/Program.cs
@@ -7,30 +7,28 @@ namespace ExitCycles
         static void Main(string[] args)  
         {
             int orderKitchensProducts;   
-            string keyWord = "exit";
+            string keyWord;
             string companyOrderOnline = "Amar";
             bool isKeyWord = false;
 
             Console.WriteLine($"Добро пожаловать в интернет магазин {companyOrderOnline} - все для кухни!");
-            Console.WriteLine($"Для предотвращения покупок, введите ключевое слово - {keyWord}");
+            Console.WriteLine("Для предотвращения покупок, введите ключевое слово - exit");
             Console.Write("Введите, сколько вы хотите приобрести товара: ");
-            orderKitchensProducts = Convert.ToInt32(Console.ReadLine());
-            keyWord = Console.ReadLine();
+             orderKitchensProducts = Convert.ToInt32(Console.ReadLine());
+          
 
             while (true)
             {
+                Console.Write("Введите слово exit, если хотите выйти: ");
+                keyWord = Console.ReadLine();
                 Console.WriteLine($"Вы приобрели {orderKitchensProducts++} товара");
                 Console.ReadKey();
-                if(keyWord != null)
+                
+                if (keyWord == "exit")
                 {
-                    Console.WriteLine("Завершаем программу");
+                    Console.Write("Вы вышли из программы!");
                     break;
                 }
-                else
-                {
-                    Console.Write("Продолжаем!");
-                }
-
             }
         }
     }
