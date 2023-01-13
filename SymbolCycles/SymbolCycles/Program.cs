e39e01069deb2ee0359041a776cd2f02006f1b2e
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-13 22:47:58 +0400

    SymbolCycles
    ДЗ по циклам - 7е задание

---
+++ SymbolCycles/SymbolCycles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CyclesSymbol
+{
+    class CyclesSym
+    {
+        static void Main(string[] args)
+        {
+            string userInputName;
+            var userInputSymbol = "";
+            int x;
+            int y;
+            int lenghtString;
+            int userInputN;
+
+
+            
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.SetCursorPosition(x, y);
+            Console.Write("Введите имя: ");
+            userInputName = Console.ReadLine();
+            Console.WriteLine( 1 + (userInputName.Length) + 1);
+            userInputN = userInputName.Length;
+            Console.Write("Введите символ для прикола: ");
+            userInputSymbol = Console.ReadLine();
+
+            
+        }
+    }
+}
