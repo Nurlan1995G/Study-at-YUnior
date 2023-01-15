d57871e1ccdc4404fcb0b6a2f9c07b5ee094ec83
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-15 16:43:48 +0400

    FindCountNumbersCycles
    Дано N (1 ≤ N ≤ 27). Найти количество трехзначных натуральных чисел, которые кратны N. Операции деления (/, %) не использовать. А умножение не требуется.
    Число N всего одно, его надо получить в нужном диапазоне.

---
+++ FindCountNumberCycles/FindCountNumberCycles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FindCycles
+{
+    class Cycles    
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int receivedNumber;  
+            int smallNumber = 1;
+            int bigNumber = 27;
+
+            receivedNumber = random.Next(smallNumber, bigNumber);
+
+            while (receivedNumber > 0)
+            {
+                receivedNumber = random.Next(smallNumber, bigNumber + 1);
+                Console.WriteLine(receivedNumber);
+                Console.ReadKey();
+
+                if (smallNumber < receivedNumber && receivedNumber < bigNumber)
+                {
+                    Console.WriteLine($"{smallNumber} < {receivedNumber} < {bigNumber}");
+                }
+                else if(smallNumber == receivedNumber && receivedNumber < bigNumber)
+                {
+                    Console.WriteLine($"{smallNumber} = {receivedNumber} < {bigNumber}");
+                }
+                else if(smallNumber < receivedNumber && receivedNumber == bigNumber)
+                {
+                    Console.WriteLine($"{smallNumber} < {receivedNumber} = {bigNumber}");
+                }
+            }
+        }
+    }
+}
