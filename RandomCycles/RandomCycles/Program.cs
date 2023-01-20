af573b5984c43fb4cc0815aa4bec1b3aaade1f00
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-20 10:06:02 +0400

    RandomCycles 2.4
    С помощью Random получить число number, которое не больше 100. Найти сумму всех положительных чисел меньше number (включая число), которые кратные 3 или 5. (К примеру, это числа 3, 5, 6, 9, 10, 12, 15 и т.д.)

--- RandomCycles/RandomCycles/Program.cs
+++ RandomCycles/RandomCycles/Program.cs
@@ -2,37 +2,28 @@
 
 namespace RandonCycles
 {
-    class Cycles        
+    class Cycles    
     {
         static void Main(string[] args)
         {
             Random random = new Random();
             int numbersSum = 0;
             int randomNumber;
-            int endingNumber = 100;
+            int endingNumber = 101;
             int divisibleNumber3 = 3;
             int divisibleNumber5 = 5;
-            int userInputNumber;
 
             randomNumber = random.Next(0, endingNumber);
             Console.WriteLine($"Выпало рандомное число - {randomNumber}");
-            Console.Write($"Ведите кратное число {divisibleNumber3} или {divisibleNumber5}: ");
-            userInputNumber = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i <= endingNumber; i++)
+            for (int i = 0; i <= randomNumber; i++)
             {
-                if (userInputNumber == divisibleNumber5 && randomNumber >= numbersSum)
+                if(i % divisibleNumber5 == 0 || i % divisibleNumber3 == 0)
                 {
-                    numbersSum += divisibleNumber5;
-                    Console.WriteLine(numbersSum);
-                }
-
-                if(userInputNumber == divisibleNumber3 && randomNumber >= numbersSum)
-                {
-                    numbersSum += divisibleNumber3;
-                    Console.WriteLine(numbersSum);
+                    numbersSum += i;
                 }
             }
+            Console.WriteLine($"Сумма положительных чисел, которые кратны {divisibleNumber3} или {divisibleNumber5} - равна {numbersSum}");
         }
     }
 }
