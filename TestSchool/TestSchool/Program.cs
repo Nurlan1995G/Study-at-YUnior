9050f341444077fb57f7a6362953dfb1da0407dc
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-21 17:51:05 +0400

    TestShool
    Вывести имя в прямоугольник из символа, который введет сам пользователь.
    
    Вы запрашиваете имя, после запрашиваете символ, а после отрисовываете в консоль его имя в прямоугольнике из его символов.
    
    Пример:
    
    Alexey
    
    %
    
    %%%%%%
    % Alexey %
    %%%%%%

---
+++ TestSchool/TestSchool/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSLight    
+{
+    class Program   
+    {
+        static void Main(string[] args)
+        {
+            string userInputName;
+            char userInputSymbol;
+            int beforyName = 1;
+            int averageValue = 2;
+            string connectingWords;
+            char userSmbol;
+
+            Console.Write("Введите имя: ");
+            userInputName = Console.ReadLine();
+            Console.Write("Введите любой символ: ");
+            userInputSymbol = Convert.ToChar(Console.ReadLine());
+            connectingWords = $"{userInputSymbol} {userInputName}  {userInputSymbol}";
+            userSmbol = userInputSymbol;
+
+            for (int i = 0; i < userInputName.Length; i++)
+            {
+                Console.Write($"{userInputSymbol} ");
+                userSmbol = userInputSymbol;
+            }
+
+            Console.WriteLine(userSmbol);
+            Console.WriteLine(connectingWords);
+            Console.WriteLine(userSmbol);
+        }
+    }
+}
