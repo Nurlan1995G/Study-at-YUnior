b1f7f2446d18a81e843b4117ce30af180a3ba7bd
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-21 23:56:13 +0400

    DegreeTwoCycles 2.10
    Найдите минимальную степень двойки, превосходящую заданное число.
    К примеру, для числа 4 будет 2 в степени 3, то есть 8. 4<8.
    Для числа 29 будет 2 в степени 5, то есть 32. 29<32.
    В консоль вывести число (лучше получить от Random), степень и само число 2 в найденной степени.

---
+++ DegreeTwoCycles2.10/DegreeTwoCycles2.10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DegreeTwo
+{
+    class Cysles   
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber;
+            int degreeNumber = 0;
+            int mainNumber = 2;
+            int resultingValue = 1;  
+            int minValueRandomNumbers = 1;
+            int maxValueRandomNumbers = 101;
+
+            randomNumber = random.Next(minValueRandomNumbers,maxValueRandomNumbers);
+            Console.WriteLine($"Выпало число - {randomNumber}");
+
+            for(int i = 0; i < randomNumber; i++)
+            {
+                if(randomNumber >= resultingValue)
+                {
+                    resultingValue *= mainNumber;
+                    degreeNumber++;
+                }
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine($"Число - {randomNumber}. Минимальная степень двойки превосходящая заданое число, будет {mainNumber} в степень {degreeNumber} равна - {resultingValue}.\n {randomNumber} < {resultingValue}");
+        }
+    }
+}
