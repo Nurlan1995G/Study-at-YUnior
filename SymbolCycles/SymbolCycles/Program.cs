7682fdcdb29bbb6d5705d47294b825c5e43488a0
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-21 23:36:21 +0400

    SymbolCycles 2.7
    Вывести имя в прямоугольник из символа, который введет сам пользователь.
    
    Вы запрашиваете имя, после запрашиваете символ, а после отрисовываете в консоль его имя в прямоугольнике из его символов.
    
    Пример:
    
    Alexey
    
    %
    
    %%%%%%
    % Alexey %
    %%%%%%
    
    Примечание:
    
    Длину строки можно всегда узнать через свойство Length
    
    string someString = “Hello”;
    
    Console.WriteLine(someString.Length); //5

--- SymbolCycles/SymbolCycles/Program.cs
+++ SymbolCycles/SymbolCycles/Program.cs
@@ -7,31 +7,24 @@ namespace CyclesSymbol
         static void Main(string[] args)
         {
             string userInputName;
-            char userInputSymbol;
-            int beforyName = 1;
-            int averageValue = 2;
+            string userInputSymbol;
             string connectingWords;
+            string receivedSymbol = "";
 
             Console.Write("Введите имя: ");
             userInputName = Console.ReadLine();
             Console.Write("Введите любой символ: ");
-            userInputSymbol = Convert.ToChar(Console.ReadLine());
-            connectingWords = $"{userInputSymbol} {userInputName}  {userInputSymbol}";
+            userInputSymbol = Console.ReadLine();
+            connectingWords = userInputSymbol + userInputName + userInputSymbol;
 
-            for(int i = 0; i < averageValue; i++)
+            for (int i = 0; i < userInputName.Length; i++)
             {
-                if(i == beforyName)
-                {
-                    Console.WriteLine($"{connectingWords}");
-                }
-
-                for(int j = 0; j < userInputName.Length; j++)
-                {
-                    Console.Write($"{userInputSymbol} ");
-                }
-
-                Console.WriteLine();
+                receivedSymbol += userInputSymbol;
             }
+
+            Console.WriteLine(receivedSymbol);
+            Console.WriteLine(connectingWords);
+            Console.WriteLine(receivedSymbol);
         }
     }
 }
