0afcdf37e34bc87e83aa9b1fc9a93b5913d39ad5
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-22 20:58:18 +0400

    WorkingSpecificColumnsArray 3.1
    Дан двумерный массив.
    Вычислить сумму второй строки и произведение первого столбца. Вывести исходную матрицу и результаты вычислений.

---
+++ WorkingSpecificColumnsArray 3.1/WorkingSpecificColumnsArray 3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WorkingArray
+{
+    class Working   
+    {
+        static void Main(string[] args)
+        {
+            int[,] fridge = { {2,5,1 }, {3,6,2 }, {4,1,7 } };
+
+            for(int i = 0; i < fridge.GetLength(0); i++)
+            {
+                for(int j = 0; j < fridge.GetLength(1); j++)
+                {
+                    Console.Write(fridge[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write($"\nСумма второй строки,первого столбца равна - {fridge[1, 0]}\n");
+        }
+    }
+}
