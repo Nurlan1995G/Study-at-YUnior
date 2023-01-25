c3b1c8a21345cee74bf71c16477483a037164c2c
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-25 11:05:15 +0400

    WorkingSpesificColumnsArray 3.1
    Дан двумерный массив.
    Вычислить сумму второй строки и произведение первого столбца. Вывести исходную матрицу и результаты вычислений.

--- WorkingSpecificColumnsArray 3.1/WorkingSpecificColumnsArray 3.1/Program.cs
+++ WorkingSpecificColumnsArray 3.1/WorkingSpecificColumnsArray 3.1/Program.cs
@@ -2,22 +2,42 @@
 
 namespace WorkingArray
 {
-    class Working   
+    class Working    //Дан двумерный массив. Вычислить сумму второй строки и произведение первого столбца.Вывести исходную матрицу и результаты вычислений.
     {
         static void Main(string[] args)
         {
+            bool isOpen = true;
             int[,] fridge = { {2,5,1 }, {3,6,2 }, {4,1,7 } };
+            int number = 0;
+            int additionSum = 0;
+            int productSum = 1;
 
-            for(int i = 0; i < fridge.GetLength(0); i++)
+            while (isOpen)
             {
-                for(int j = 0; j < fridge.GetLength(1); j++)
+                for (int i = 1; i < fridge.GetLength(0); i++)
                 {
-                    Console.Write(fridge[i,j] + " ");
+                    if (i == 1)
+                    {
+                        Console.WriteLine("Закончился цикл по i");
+                    }
+
+                    if(i == 2)
+                    {
+                        isOpen = false;
+                        break;
+                    }
+
+                    for (int j = 0; j < fridge.GetLength(1); j++)
+                    {
+                        number = fridge[i,j];
+                        additionSum += number;
+                        productSum *= number;
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
             }
 
-            Console.Write($"\nСумма второй строки,первого столбца равна - {fridge[1, 0]}\n");
+            Console.Write($"\nСумма второй строки равна - {additionSum},произведение первого столбца равна - {productSum}\n");
         }
     }
 }
