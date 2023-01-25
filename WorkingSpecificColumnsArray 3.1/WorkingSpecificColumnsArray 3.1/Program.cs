2b145cd5f19e31984c079b08323e5ae52b44fc39
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-25 23:06:13 +0400

    WorkingSpecificColumnsArray 3.1
    Дан двумерный массив.
    Вычислить сумму второй строки и произведение первого столбца. Вывести исходную матрицу и результаты вычислений.

--- WorkingSpecificColumnsArray 3.1/WorkingSpecificColumnsArray 3.1/Program.cs
+++ WorkingSpecificColumnsArray 3.1/WorkingSpecificColumnsArray 3.1/Program.cs
@@ -9,13 +9,12 @@ namespace WorkingArray
             int[,] fridge = { {2,5,1 }, {3,6,2 }, {4,1,7 } };
             int additionSum = 0;
             int productSum = 1;
-            int column = 1;
-            int valueZero = 0;
-            int row = 1;
+            int columnNumber = 1;
+            int rowNumber = 1;
 
-            for (int i = 0; i < fridge.GetLength(valueZero); i++) 
+            for (int i = 0; i < fridge.GetLength(0); i++) 
             {
-                for (int j = 0; j < fridge.GetLength(1); j++)
+                for (int j = 0; j < fridge.GetLength(columnNumber); j++)
                 {
                     Console.Write(fridge[i, j] + " ");
                 }
@@ -23,14 +22,14 @@ namespace WorkingArray
                 Console.WriteLine();
             }
 
-            for(int i = 0; i < fridge.GetLength(valueZero); i++)
+            for(int i = 0; i < fridge.GetLength(0); i++)
             {
-                productSum *= fridge[i,valueZero];
+                productSum *= fridge[i,0];
             }
 
-            for(int j = 0; j < fridge.GetLength(column); j++)
+            for(int j = 0; j < fridge.GetLength(columnNumber); j++)
             {
-                additionSum += fridge[row,j];
+                additionSum += fridge[rowNumber,j];
             }
               
             Console.Write($"\nСумма второй строки равна - {additionSum},произведение первого столбца равна - {productSum}\n");
