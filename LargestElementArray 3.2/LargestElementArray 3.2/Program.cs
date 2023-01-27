367edc7322ac1dd90536c9656c6a7a744d9cf47c
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-27 14:36:20 +0400

    LargestElementArray 3.2

--- LargestElementArray 3.2/LargestElementArray 3.2/Program.cs
+++ LargestElementArray 3.2/LargestElementArray 3.2/Program.cs
@@ -2,32 +2,31 @@
 
 namespace LargestElement
 {
-    class Array   //Найти наибольший элемент матрицы A(10,10) и записать ноль в те ячейки, где он находятся. Вывести наибольший элемент, исходную и полученную матрицу. Массив под измененную версию не нужен.
+    class Array   
     {
         static void Main(string[] args)
         {
             Random random = new Random();
             int[,] elementMatrix = new int[10,10];
-            int numbers = 0;
             int maxElement = int.MinValue;
-
-            Console.SetCursorPosition(0, 15);
+            int startNumberMatrix = 0;
 
             for(int i = 0; i < elementMatrix.GetLength(0); i++)
             {
                 for(int j = 0; j < elementMatrix.GetLength(1); j++)
                 {
-                    elementMatrix[i, j] = random.Next(0, 10);
+                    elementMatrix[i, j] = random.Next(0, 9);
                     Console.Write(elementMatrix[i, j] + " ");
 
-                    if(maxElement < elementMatrix[i, j])
+                    if(maxElement <= elementMatrix[i, j])
                     {
                         maxElement = elementMatrix[i, j];
                     }
                 }
 
+                Console.Write(" " + maxElement);
                 Console.WriteLine();
-                Console.WriteLine(maxElement);
+                maxElement = startNumberMatrix;
             }
         }
     }
