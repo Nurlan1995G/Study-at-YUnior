faeb8761af01772c1c7b68cbb33b14d389abedea
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-28 16:52:08 +0400

    LocalMaximaArray 3.3

---
+++ LocalMaximaArray 3.3/LocalMaximaArray 3.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LocalMaxima
+{
+    class Local   //Дан одномерный массив целых чисел из 30 элементов. Найдите все локальные максимумы и вывести их. (Элемент является локальным максимумом, если он не имеет соседей, больших, чем он сам)
+                  //Крайние элементы являются локальными максимумами если не имеют соседа большего, чем они сами.  Программа должна работать с массивом любого размера.  Массив всех локальных максимумов не нужен.
+    {
+        static void Main(string[] args)
+        {
+            int[] fullNumbers = new int[30];
+            Random random = new Random();
+            int maxElement = int.MinValue;
+            int number = 0;
+
+            for(int i = 0; i < fullNumbers.Length; i++)
+            {
+                fullNumbers[i] = random.Next(0,9);
+                Console.Write(fullNumbers[i] + " ");
+                maxElement = fullNumbers[i];
+
+                if (fullNumbers[i] < maxElement && maxElement > fullNumbers[i])
+                {
+                    Console.Write($"Локальнам максимум является элемент - {fullNumbers[i]}");
+                }
+            }
+        }
+    }
+}
