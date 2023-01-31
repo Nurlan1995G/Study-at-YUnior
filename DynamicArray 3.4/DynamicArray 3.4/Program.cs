e77c65b948fc78fd967160ac5813891938e3cfb7
Nurlan19958 <www.nurlan1995@mail.ru>
2023-01-31 20:50:33 +0400

    DynamicArray 3.4
    Пользователь вводит числа, и программа их запоминает.
    Как только пользователь введёт команду sum, программа выведет сумму всех веденных чисел.
    
    Выход из программы должен происходить только в том случае, если пользователь введет команду exit.
    Если введено не sum и не exit, значит это число и его надо добавить в массив.
    
    Программа должна работать на основе расширения массива.
    
    Внимание, нельзя использовать List<T> и Array.Resize

---
+++ DynamicArray 3.4/DynamicArray 3.4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DynamicArray
+{
+    class Dynamic  
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sumNumbers = 0;
+            string userInputSum = "sum";
+            string userInputExit = "exit";
+            string userInputString;
+            bool isWorkProgress = true;
+
+            Console.WriteLine($"Вы можете тут водить числа и после того как введете команду - {userInputSum}, выведется сумма ваших чисел. Либо вести команду - {userInputExit}, выполнится выход из программы!");
+
+            while (isWorkProgress)
+            {
+                Console.Write("Введите число: ");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                int[] userInputNumbers = { number };
+
+                Console.Write($"Введите комманду - {userInputSum} или {userInputExit}: ");
+                userInputString = Console.ReadLine();
+
+                for(int i = 0; i < userInputNumbers.Length; i++)
+                {
+                    sumNumbers += userInputNumbers[i];
+
+                    if(userInputString == userInputSum)
+                    {
+                        Console.WriteLine($"Сумма числен равна - {sumNumbers}");
+                    }
+                }
+
+                if(userInputString == userInputExit)
+                {
+                    Console.WriteLine("Вы вели команду завершения программы!");
+                    isWorkProgress = false;
+                }
+            }
+        }
+    }
+}
