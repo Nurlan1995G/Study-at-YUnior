d43187a8ee888bdaaf38d0d86147377f9df29001
Nurlan19958 <www.nurlan1995@mail.ru>
2023-02-07 13:08:42 +0400

    ArrayRepetitionsNumber 3.5
    В массиве чисел найдите самый длинный подмассив из одинаковых чисел.
    Дано 30 чисел. Вывести в консоль сам массив, число, которое само больше раз повторяется подряд и количество повторений.
    Дополнительный массив не надо создавать.
    Пример: {5, 5, 9, 9, 9, 5, 5} - число 9 повторяется большее число раз подряд.

--- ArrayRepetitionsNumber 3.5/ArrayRepetitionsNumber 3.5/Program.cs
+++ ArrayRepetitionsNumber 3.5/ArrayRepetitionsNumber 3.5/Program.cs
@@ -2,46 +2,44 @@
 
 namespace RepetitionsNumber
 {
-    class ArrayNumber  // В массиве чисел найдите самый длинный подмассив из одинаковых чисел. Дано 30 чисел.Вывести в консоль сам массив, число, которое само больше раз повторяется подряд и количество повторений. Дополнительный массив не надо создавать.
-                       // Пример: {5, 5, 9, 9, 9, 5, 5} - число 9 повторяется большее число раз подряд.
+    class ArrayNumber  
     {
         static void Main(string[] args)
         {
             int[] numbers = new int[30];
             Random random = new Random();
             int number = 1;
-            int num = int.MinValue;
+            int receivedNumber = 0;
+            int maxValue = int.MinValue;
+            int startIteration = 1;
+            int originalNumber = 1;
 
             for(int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = random.Next(1,9);
                 Console.Write(numbers[i] + " ");
+
+                if (i > startIteration)
+                {
+                    if (numbers[i] == numbers[i - 1])
+                    {
+                        number++;
+                    }
+                    else
+                    {
+                        number = originalNumber;
+                    }
+
+                    if (maxValue < number)
+                    {
+                        maxValue = number;
+                        receivedNumber = numbers[i];
+                    }
+                }
             }
 
             Console.WriteLine("\n");
-
-            for (int i = 1; i < numbers.Length; i++)
-            {
-                if (numbers[i] == numbers[i - 1])
-                {
-                    number++;
-                }
-                else if(number == 1)
-                {
-                    continue;
-                }
-                else
-                {
-                    Console.Write($"Число - {numbers[i - 1]} повторяется {number} раза; ");
-                    number = 1;
-                }
-            }
-
-            if (numbers[numbers.Length - 1] == numbers[numbers.Length - 2])
-            {
-                Console.Write($"Число - {numbers[numbers.Length - 1]} повторяется {number} раза; ");
-            }
-
+            Console.WriteLine($"Число - {receivedNumber} повторяется {maxValue} раз");
             Console.WriteLine();
         }
     }
