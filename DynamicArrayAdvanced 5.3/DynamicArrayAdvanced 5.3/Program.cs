a85e6e11a76abb8b7ea2fe7968be4db251367dee
Nurlan19958 <www.nurlan1995@mail.ru>
2023-02-18 19:22:12 +0400

    DynamicArrayAdvanced 5.3
    В массивах вы выполняли задание "Динамический массив"
    Используя всё изученное, напишите улучшенную версию динамического массива(не обязательно брать своё старое решение)
    Задание нужно, чтобы вы освоились с List и прощупали его преимущество.
    Проверка на ввод числа обязательна.
    
    Пользователь вводит числа, и программа их запоминает.
    Как только пользователь введёт команду sum, программа выведет сумму всех веденных чисел.
    
    Выход из программы должен происходить только в том случае, если пользователь введет команду exit.

---
+++ DynamicArrayAdvanced 5.3/DynamicArrayAdvanced 5.3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DynamicArrayAdvanced
+{
+    class DynamicArray  
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            string userInputSum = "sum";
+            string userInputExit = "exit";
+            string userInput;
+            int number;
+            bool isWorkProgress = true;
+
+            Console.WriteLine($"Вы можете тут водить числа и после того как введете команду - {userInputSum}, выведется сумма ваших чисел. Либо вести команду - {userInputExit}, выполнится выход из программы!");
+
+            while (isWorkProgress)
+            {
+                Console.Write("Введите любую команду: ");
+                userInput = Console.ReadLine();
+
+                if(userInput == userInputExit)
+                {
+                    Console.WriteLine("Вы вели команду выхода из программы!");
+                    isWorkProgress = false;
+                }
+                else if(userInput == userInputSum)
+                {
+                    int receivedSum = 0;
+
+                    for(int i = 0; i < numbers.Count; i++)
+                    {
+                        receivedSum += numbers[i];
+                    }
+
+                    Console.WriteLine($"Сумма чисел равна - {receivedSum}");
+                }
+                else
+                {
+                    number = int.Parse(userInput);
+                    numbers.Add(number);
+                }
+            }
+        }
+    }
+}
