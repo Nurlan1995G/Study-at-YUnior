6162183fb7a183fcf78b0519e8c1b009cc3b4708
Nurlan19958 <www.nurlan1995@mail.ru>
2023-02-25 11:57:58 +0400

    QueueAtStore 5.2
    У вас есть множество целых чисел. Каждое целое число - это сумма покупки.
    Вам нужно обслуживать клиентов до тех пор, пока очередь не станет пуста.
    После каждого обслуженного клиента деньги нужно добавлять на наш счёт и выводить его в консоль.
    После обслуживания каждого клиента программа ожидает нажатия любой клавиши, после чего затирает консоль и по новой выводит всю информацию, только уже со следующим клиентом

---
+++ QueueAtStore 5.2/QueueAtStore 5.2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace QueueAtStore
+{
+    class AtStore  
+    {
+        static void Main(string[] args)
+        {
+            int balanceBuys = 0;
+            Queue<int> buys = new Queue<int>();
+            bool isWorking = true;
+
+            CreateQueue(buys);
+
+            while (isWorking)
+            {
+                if (buys.Count > 0)
+                {
+                    SubstractBuy(buys, ref balanceBuys);
+                    ShowQueue(buys);
+                }
+                else
+                {
+                    Console.WriteLine($"Очередь закончилась. Баланс составляется - {balanceBuys} долларов");
+                    isWorking = false;
+                }
+            }
+        }
+
+        static void CreateQueue (Queue<int> buys)
+        {
+            Random random = new Random();
+            int randomQueue = random.Next(5, 11);
+            int randomNumberBuys = 0;
+
+            for (int i = 0; i < randomQueue; i++)
+            {
+                randomNumberBuys = random.Next(1, 21);
+                buys.Enqueue(randomNumberBuys);
+            }
+
+            Console.WriteLine($"Длина очереди - {buys.Count} покупателей");
+
+            foreach (int buy in buys)
+            {
+                Console.WriteLine($"Сумма покупки - {buy}$");
+            }
+
+            Console.Write("Нажмите на любую клавишу для продолжения: ");
+            Console.ReadKey();
+        }
+
+        static void SubstractBuy(Queue<int> buys, ref int balanceBuys)
+        {
+            Console.Clear();
+            balanceBuys += buys.Dequeue();
+            Console.WriteLine($"Баланс - {balanceBuys}$");
+        }
+
+        static void ShowQueue(Queue<int> buys)
+        {
+            foreach (int buyNum in buys)
+            {
+                Console.WriteLine($"Сумма покупки - {buyNum}$");
+            }
+
+            Console.Write("Нажмите на любую клавишу для продолжения: ");
+            Console.ReadKey();
+        }
+    }
+}
