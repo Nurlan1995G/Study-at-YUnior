1225dd58213b707f099c1e944a097e2582703a25
Nurlan19958 <www.nurlan1995@mail.ru>
2023-02-25 17:14:59 +0400

    ParenthesisExpressionsCycles 2.11
    Дана строка из символов '(' и ')'. Определить, является ли она корректным скобочным выражением. Определить максимальную глубину вложенности скобок.
    
    Пример “(()(()))” - строка корректная и максимум глубины равняется 3.
    Пример не верных строк: "(()", "())", ")(", "(()))(()"
    
    Для перебора строки по символам можно использовать цикл foreach, к примеру будет так foreach (var symbol in text)
    Или цикл for(int i = 0; i < text.Length; i++) и дальше обращаться к каждому символу внутри цикла как text[i]
    Цикл нужен для перебора всех символов в строке.

---
+++ ParenthesisExpressionsCycles 2.11/ParenthesisExpressionsCycles 2.11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PerenthesisCycles
+{
+    class Cycles  
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            var symbolLeftParenthesis = '(';
+            var symbolRightParenthesis = ')';
+            int countLeftParenthesis = 0;
+            int countRightParenthesis = 0; 
+            int countSymbol = 0;
+
+            Console.Write("Введите скобочные выражение и узнаете, верныли ли они, а также их глубину : ");
+            userInput = Console.ReadLine();
+
+            for(int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] == symbolLeftParenthesis)
+                {
+                    countLeftParenthesis++;
+                    countSymbol++;
+                }
+                else if(userInput[i] == symbolRightParenthesis)
+                {
+                    countRightParenthesis++;   
+                }
+            }
+
+            if(countLeftParenthesis == countRightParenthesis && userInput[0] != ')' && userInput.Length - 1 != '(')
+            {
+                Console.WriteLine($"\nВы вели коректное скобочное выражение и глубина ее составляет - {countSymbol}");
+            }
+            else
+            {
+                Console.WriteLine("Скобочное выражение не верное!");
+            }
+        }
+    }
+}
