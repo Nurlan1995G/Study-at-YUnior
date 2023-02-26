b1faa692b63e9aaf87505422848ca384e589fb50
Nurlan19958 <www.nurlan1995@mail.ru>
2023-02-26 13:26:09 +0400

    OOPKlass 6.1
    Создать класс игрока, с полями, содержащими информацию об игроке и методом, который выводит информацию на экран.
    
    В классе обязательно должен быть конструктор

---
+++ OOPKlass 6.1/OOPKlass 6.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Nurlan", 27, 'M');
+
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _age;
+        private char _gender;
+
+        public Player(string name, int age, char gender)
+        {
+            _name = name;    
+            _age = age;  
+            _gender = gender;    
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Игрок: \nИмя - {_name}, \nВозраст - {_age}, \nПол - {_gender}");
+        }
+    }
+}
