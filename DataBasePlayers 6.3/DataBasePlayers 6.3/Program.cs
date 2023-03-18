1c8b43e41118e65862ed55dfd83c24ca0a8fb5c9
Nurlan19958 <www.nurlan1995@mail.ru>
2023-03-18 14:59:36 +0400

    DataBasePlayers 6.3
    Реализовать базу данных игроков и методы для работы с ней.
    У игрока может быть уникальный номер, ник, уровень, флаг – забанен ли он(флаг - bool).
    Реализовать возможность добавления игрока, бана игрока по уникальный номеру, разбана игрока по уникальный номеру и удаление игрока.
    Создание самой БД не требуется, задание выполняется инструментами, которые вы уже изучили в рамках курса. Но нужен класс, который содержит игроков и её можно назвать "База данных".

--- DataBasePlayers 6.3/DataBasePlayers 6.3/Program.cs
+++ DataBasePlayers 6.3/DataBasePlayers 6.3/Program.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Reflection.Metadata;
-using System.Collections.Generic;
-
-namespace DataBasePlayers
+﻿namespace DataBasePlayers
 {
-    class Program  //Реализовать базу данных игроков и методы для работы с ней.
-                   //У игрока может быть уникальный номер, ник, уровень, флаг – забанен ли он(флаг - bool).
-                   //Реализовать возможность добавления игрока, бана игрока по уникальный номеру, разбана игрока по уникальный номеру и удаление игрока.
-                   //Создание самой БД не требуется, задание выполняется инструментами, которые вы уже изучили в рамках курса.Но нужен класс, который содержит игроков и её можно назвать "База данных".
+    class Program 
     {
         static void Main(string[] args)
         { 
@@ -25,44 +18,97 @@ namespace DataBasePlayers
             {
                 Console.WriteLine($"Введите команду с меню!");
                 Console.WriteLine($"\n{AddPlayer} - Добавление персонажа;\n{BanPlayer} - Забанить персонажа;\n{UnBanPlayer} - Разбанить персонажа;\n{DeletePlayer} - Удалить персонажа;\n{ShowInfoPlayer} - Вывод информации о всех персонажах;\n{Exit} - выход из программы.");
-
                 string userInput = Console.ReadLine();
-                bool isUserNumber = int.TryParse(userInput, out int number);
+                bool isNumber = int.TryParse(userInput, out int number);
 
-                if (userInput == AddPlayer)
+                if (isNumber)
                 {
-                    dataBase.AddPlayer();
-                }
-                else if (userInput == BanPlayer)
-                {
-                    dataBase.BanPlayer();
-                }
-                else if (userInput == UnBanPlayer)
-                {
-                    dataBase.UnBanPlayer();
-                }
-                else if (userInput == DeletePlayer)
-                {
-                    dataBase.DeletePlayer();
-                }
-                else if(userInput == ShowInfoPlayer)
-                {
-                    dataBase.ShowAllInfoPlayer();
-                }
-                else if (userInput == Exit)
-                {
-                    isWorking = false;
+                    if (userInput == AddPlayer)
+                    {
+                        dataBase.AddPlayer();
+                    }
+                    else if (userInput == BanPlayer)
+                    {
+                        dataBase.BanPlayer();
+                    }
+                    else if (userInput == UnBanPlayer)
+                    {
+                        dataBase.UnBanPlayer();
+                    }
+                    else if (userInput == DeletePlayer)
+                    {
+                        dataBase.DeletePlayer();
+                    }
+                    else if (userInput == ShowInfoPlayer)
+                    {
+                        dataBase.ShowAllInfoPlayer();
+                    }
+                    else if (userInput == Exit)
+                    {
+                        Console.WriteLine("\nВы вели команду выхода из программы!");
+                        isWorking = false;
+                    }
                 }
                 else
                 {
-                    isUserNumber = false;
-                    Console.WriteLine("Вы вели некоректную команду!");
-                    Console.ReadLine();
-                    Console.Clear();
+                    Console.WriteLine("\nЧто-то вы вели не то. Попробуйте еще раз!\n");
+                    Console.ReadKey();
                 }
+
+                Console.WriteLine("Нажмите на любую клавишу");
+                Console.ReadKey();
+                Console.Clear();
             }
         }
     }
+
+    class Player
+    {
+        private static int _number;
+
+        public string NickName { get; private set; }
+        public int LevelPlayer { get; private set; }
+        public bool isBan { get; private set; }
+        public int UniqueNumberPlayer { get; private set; }
+
+        public Player(string nickName, int levelPlayer)
+        {
+            NickName = nickName;
+            LevelPlayer = levelPlayer;
+            isBan = false;
+            CreateNumber();
+        }
+
+        public void CreateNumber()
+        {
+            UniqueNumberPlayer = ++_number;
+        }
+
+        public void Ban()
+        {
+            isBan = true;
+        }
+
+        public void UnBan()
+        {
+            isBan = false;
+        }
+
+        public void ShowInfo()
+        {
+            if (isBan)
+            {
+                Console.WriteLine("\nПользователь забанен!");
+            }
+            else
+            {
+                Console.WriteLine("\nНе забанен!");
+            }
+
+            Console.WriteLine($"Уникальный номер - {UniqueNumberPlayer} | Ник - {NickName} | уровень - {LevelPlayer}");
+        }
+    }
+
 
     class DataBase
     {
@@ -71,47 +117,51 @@ namespace DataBasePlayers
 
         public void AddPlayer()
         {
-            Console.Write("Введите ник вашего персонажа: ");
-            string userInputNick = Console.ReadLine();
-            int level = random.Next(1, 11);
+            Console.Write("Введите ваш ник:");
+            string nickPlayer = Console.ReadLine();
+            bool isnickPlayer = int.TryParse(nickPlayer, out int number);
+            int minValue = 1;
+            int maxValue = 11;
 
-            _players.Add(new Player(userInputNick, level));
+            int levelPlayer = random.Next(minValue, maxValue);
+
+            _players.Add(new Player(nickPlayer, levelPlayer));
         }
 
         public void DeletePlayer()
         {
-            if(TryGetPlayer(out Player player))
+            if(GetPlayer(out Player player))
             {
                 _players.Remove(player);
-                Console.WriteLine("\nПользователь удален!\n");
+                Console.WriteLine($"\nИгрок с номером {player.UniqueNumberPlayer} удален!");
             }
             else
             {
-                Console.WriteLine("Такого персонажа нет!");
+                Console.WriteLine("Такого номера нет!");
             }
         }
 
         public void BanPlayer()
         {
-            if(TryGetPlayer(out Player player))
+            if(GetPlayer(out Player player))
             {
                 player.Ban();
             }
             else
             {
-                Console.WriteLine("Такого персонажа нет!");
+                Console.WriteLine("Такого пользователья нет. Нет возможности забанить!");
             }
         }
 
         public void UnBanPlayer()
         {
-            if(TryGetPlayer(out Player player))
+            if(GetPlayer(out Player player))
             {
                 player.UnBan();
             }
             else
             {
-                Console.WriteLine("Такого персонажа нет!");
+                Console.WriteLine("Разбанить не получилось, может его просто нет!");
             }
         }
 
@@ -122,83 +172,39 @@ namespace DataBasePlayers
                 foreach (Player player in _players)
                 {
                     player.ShowInfo();
-                } 
+                }
             }
             else
             {
-                Console.WriteLine("Еще нет пользователей!");
+                Console.WriteLine("У вас пока нет ни одного игрока!");
             }
         }
 
-        public bool TryGetPlayer(out Player player)
+        public bool GetPlayer(out Player player)
         {
             player = null;
 
-            Console.Write("Введите номер игрока для продолжения операции: ");
-            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите уникальный номер игрока: ");
+            bool isNumber = int.TryParse(Console.ReadLine(), out int uniqueNumberToFind);
 
-            for(int i = 0; i < _players.Count; i++)
+            if (isNumber)
             {
-                if(userInput == _players[i].UniqueNumberPlayer)
+                for (int i = 0; i < _players.Count; i++)
                 {
-                    player = _players[i];
-                    return true;
+                    if (uniqueNumberToFind == _players[i].UniqueNumberPlayer)
+                    {
+                        player = _players[i];
+                        return true;
+                    }
                 }
-            }
-
-            return false;
-        }
-
-        public void CheckInfo()
-        {
-            
-        }
-    }
-
-
-    class Player
-    {
-        private static int _number;
-        public Player( string namePlayer, int levelPlayer)
-        {
-            NickPlayer = namePlayer;
-            LevelPlayer = levelPlayer;
-            IsBan = false;
-            IncreaseNumber();
-        }
-
-        public int UniqueNumberPlayer { get; private set; }
-        public string NickPlayer { get; private set; }
-        public int LevelPlayer { get; private set; }
-        public bool IsBan { get; private set; }
-
-        public void ShowInfo()
-        {
-            Console.WriteLine($"Уникальный номер - {UniqueNumberPlayer} | Ник пользователя - {NickPlayer} | Уровень - {LevelPlayer}");
-
-            if (IsBan)
-            {
-                Console.WriteLine("Пользователь забанен!\n");
             }
             else
             {
-                Console.WriteLine("Пользователь не забанен!\n");
+                Console.WriteLine("Вы вели неверную команду. Попробуйте еще раз!");
+                Console.ReadKey();
             }
-        }
 
-        public void Ban()
-        {
-            IsBan = true;
-        }
-
-        public void UnBan()
-        {
-            IsBan = false;
-        }
-
-        public void IncreaseNumber()
-        {
-            UniqueNumberPlayer = ++_number; ;
+            return false;
         }
     }
 }
